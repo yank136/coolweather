089b966f9ed1686f4e8815cf870f1f8bcea52e94
yank <1226332619@qq.com>
2018-02-17 16:03:48 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/yujianghu/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.example.yujianghu.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yujianghu on 2018/2/17.
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+}
